e9301ca6956eb34e45fe868b4f623be2f7f594ae
ris <iris.wong@zgm.ca>
2016-08-17 23:50:43 -0600

    update to structure.

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title</title>
+
+
+  <link href="https://fonts.googleapis.com/css?family=Unica+One" rel="stylesheet">
+  <link rel="stylesheet" href="css/vendors/normalize.css" />
+  <link rel="stylesheet" href="css/main.css" />
+
+</head>
+</html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include('includes/header.php');?>
 
 <div>
-  Hello World! 
+<h1>imaginarybeasts.net</h1>
+<h2>RISOLOGY.CA</h2>
 </div>
 <?php include('includes/footer.php');?>
