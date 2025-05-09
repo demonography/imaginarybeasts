920cfac7d1384bbaf9e966b0cbeb75093de22fd9
ris <demonography@users.noreply.github.com>
2025-05-08 18:12:00 -0700

    Update to home page and content, and 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import betterImageService from 'astro-better-image-service';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [betterImageService()]
+});
